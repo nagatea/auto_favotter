9b905deaf4b69240fda0edc47c315af553b81cd4
nagatea <cheesecakebot@yahoo.co.jp>
2017-01-21 01:13:56 +0900

    add rescue

--- auto_fav.rb
+++ auto_fav.rb
@@ -7,15 +7,20 @@ stream_client = Twitter::Streaming::Client.new do |config|
       config.access_token_secret = ENV['MY_ACCESS_TOKEN_SECRET']
 end
 
-client = Twitter::REST::Client.new do |config|
-      config.consumer_key        = ENV['MY_CONSUMER_KEY']
-      config.consumer_secret     = ENV['MY_CONSUMER_SECRET']
-      config.access_token        = ENV['MY_ACCESS_TOKEN']
-      config.access_token_secret = ENV['MY_ACCESS_TOKEN_SECRET']
-end
+begin
+      client = Twitter::REST::Client.new do |config|
+            config.consumer_key        = ENV['MY_CONSUMER_KEY']
+            config.consumer_secret     = ENV['MY_CONSUMER_SECRET']
+            config.access_token        = ENV['MY_ACCESS_TOKEN']
+            config.access_token_secret = ENV['MY_ACCESS_TOKEN_SECRET']
+      end
 
-stream_client.user do |status|
-    if status.is_a?(Twitter::Tweet)
-      client.favorite(status.id)
-    end
+      stream_client.user do |status|
+          if status.is_a?(Twitter::Tweet)
+            client.favorite(status.id)
+          end
+      end
+      
+rescue => exception
+      client.update("エラーが発生したので停止しました。")
 end
