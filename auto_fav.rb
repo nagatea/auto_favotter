516b91adc3b13cbd25b2a73d9804fe82a104ad69
nagatea <cheesecakebot@yahoo.co.jp>
2017-01-22 01:10:16 +0900

    エラーが発生したときに再起動するように変更

--- auto_fav.rb
+++ auto_fav.rb
@@ -1,4 +1,5 @@
 require "twitter"
+require "./time.rb"
 
 stream_client = Twitter::Streaming::Client.new do |config|
       config.consumer_key        = ENV['MY_CONSUMER_KEY']
@@ -7,7 +8,6 @@ stream_client = Twitter::Streaming::Client.new do |config|
       config.access_token_secret = ENV['MY_ACCESS_TOKEN_SECRET']
 end
 
-begin
       client = Twitter::REST::Client.new do |config|
             config.consumer_key        = ENV['MY_CONSUMER_KEY']
             config.consumer_secret     = ENV['MY_CONSUMER_SECRET']
@@ -15,6 +15,9 @@ begin
             config.access_token_secret = ENV['MY_ACCESS_TOKEN_SECRET']
       end
 
+begin
+      fav_retry || = 0
+      client.update("auto_favotterを起動しました。\n#{TIME_CODE}")
       stream_client.user do |status|
           if status.is_a?(Twitter::Tweet)
             client.favorite(status.id)
@@ -22,5 +25,7 @@ begin
       end
       
 rescue => exception
-      client.update("エラーが発生したので停止しました。")
+      client.update("エラーが発生したのでauto_favotterを停止しました。\n#{TIME_CODE}")
+      fav_retry += 1
+      retry if fav_retry < 5
 end
